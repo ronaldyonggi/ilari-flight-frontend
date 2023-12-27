c1656c65285b8cb64467a84990c815097ad32efd
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-12-27 17:19:32 +0800

    9.16 initial App implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react"
+import { NonSensitiveDiaryEntry } from "./types"
+import { getAllDiaries } from "./services/diaryService"
+import Diary from "./components/Diary"
 
-function App() {
+const App = () => {
+  const [diaries, setDiaries] = useState<NonSensitiveDiaryEntry[]>([])
+
+  useEffect(() => {
+    getAllDiaries()
+      .then(data => setDiaries(data))
+  }, [])
 
   return (
+    <div>
+      <h2>Diary Entries</h2>
+      {diaries.map(d => (
+        <Diary key={d.id} diaryEntry={d} />
+      ))}
+    </div>
   )
 }
 
