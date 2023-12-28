7913bbe1ce6b89f28694767f0a53f0818e3d9ee9
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-12-28 16:55:42 +0800

    fetch diaries by default have type DiaryEntry

--- src/services/diaryService.ts
+++ src/services/diaryService.ts
@@ -3,7 +3,12 @@ import { NonSensitiveDiaryEntry } from "../types"
 
 const baseUrl = 'http://localhost:3000/api/diaries'
 
-export const getAllDiaries = () => {
+const getAllDiaries = () => {
+    return axios
+        .get<DiaryEntry[]>(baseUrl)
+        .then(res => res.data)
+}
+
     return axios
         .get<NonSensitiveDiaryEntry[]>(baseUrl)
         .then(res => res.data)
