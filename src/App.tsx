e8201e5bfa2fb097fcf1027f920a4b372060399f
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-12-28 16:58:35 +0800

    9.17 App uses Form component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { DiaryEntry } from "./types"
 import diaryService from "./services/diaryService"
 import Diary from "./components/Diary"
+import Form from "./components/Form"
 
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([])
@@ -13,6 +14,9 @@ const App = () => {
 
   return (
     <div>
+      <h2>Add New Entry</h2>
+      <Form diaries={diaries} setDiaries={setDiaries} />
+
       <h2>Diary Entries</h2>
       {diaries.map(d => (
         <Diary key={d.id} diaryEntry={d} />
