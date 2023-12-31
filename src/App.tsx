767fe74d292264e5dc6e47ef2270461af3573618
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-12-31 12:52:32 +0800

    9.18 add Notification component to app

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import { DiaryEntry } from "./types"
 import diaryService from "./services/diaryService"
 import Diary from "./components/Diary"
 import Form from "./components/Form"
+import Notification from "./components/Notification"
 
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([])
+  const [message, setMessage] = useState('')
 
   useEffect(() => {
     diaryService.getAllDiaries()
@@ -15,7 +17,8 @@ const App = () => {
   return (
     <div>
       <h2>Add New Entry</h2>
-      <Form diaries={diaries} setDiaries={setDiaries} />
+      <Notification message={message}/>
+      <Form diaries={diaries} setDiaries={setDiaries} setMessage={setMessage} />
 
       <h2>Diary Entries</h2>
       {diaries.map(d => (
